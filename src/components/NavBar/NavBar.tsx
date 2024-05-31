5d09a415036cd1c1bda51b51cf0a7270f282dd7a
niketjain1 <niketj2000@gmail.com>
2024-05-31 16:55:47 +0530

    👌 IMPROVE: move the navbar into a folder

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,7 +3,7 @@ import { RiMovie2Line } from "react-icons/ri";
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row p-6 items-center justify-center bg-gray-400">
+    <div className="flex flex-row p-4 items-center justify-center bg-gray-400">
       <RiMovie2Line size={24} />
       <p className="mx-3">Movie library</p>
       <button className="ml-auto border border-gray-200 p-2 rounded-lg bg-slate-600">
