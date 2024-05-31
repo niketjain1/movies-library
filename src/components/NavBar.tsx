4f2994a9a6396c25065dbe47daa8174af155fedd
niketjain1 <niketj2000@gmail.com>
2024-05-31 12:18:50 +0530

    👌 IMPROVE: Added NavBar and customized the layout

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { RiMovie2Line } from "react-icons/ri";
+
+const NavBar = () => {
+  return (
+    <div className="flex flex-row p-6 items-center justify-center bg-gray-400">
+      <RiMovie2Line size={24} />
+      <p className="mx-3">Movie library</p>
+      <button className="ml-auto border border-gray-200 p-2 rounded-lg bg-slate-600">
+        Sign in
+      </button>
+    </div>
+  );
+};
+
+export default NavBar;
