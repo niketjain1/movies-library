48fa867fab5e44dca5ceed6a6bdaaf152fa947bb
niketjain1 <niketj2000@gmail.com>
2024-06-01 23:46:15 +0530

    📦 NEW: Added action for signup and signin

--- src/app/data/actions/registerUserAction.ts
+++ src/app/data/actions/registerUserAction.ts
@@ -1,5 +1,6 @@
 "use server";
 import { z } from "zod";
+import axiosInstance from "../../../lib/axiosInstance";
 
 const schemaRegister = z.object({
   username: z.string().min(3).max(20, {
@@ -12,6 +13,7 @@ const schemaRegister = z.object({
     message: "Please enter a valid email address",
   }),
 });
+
 export async function registerUserAction(prevState: any, formData: FormData) {
   const validatedFields = schemaRegister.safeParse({
     username: formData.get("username"),
@@ -28,8 +30,23 @@ export async function registerUserAction(prevState: any, formData: FormData) {
     };
   }
 
-  return {
-    ...prevState,
-    data: "ok",
-  };
+  try {
+    const response = await axiosInstance.post("/auth/signup", {
+      username: validatedFields.data.username,
+      password: validatedFields.data.password,
+      email: validatedFields.data.email,
+    });
+
+    return {
+      ...prevState,
+      data: response.data,
+      message: "Registration successful!",
+    };
+  } catch (error) {
+    return {
+      ...prevState,
+      error: error,
+      message: "Registration failed!",
+    };
+  }
 }
