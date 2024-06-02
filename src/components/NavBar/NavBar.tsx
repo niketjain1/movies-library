4eafeac98f26f6b319a605cc1d632e1be07b30df
niketjain1 <niketj2000@gmail.com>
2024-06-02 23:29:10 +0530

    👌 IMPROVE: added home button

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { RiMovie2Line } from "react-icons/ri";
 import CustomButton from "../button/CustomButton";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row p-4 items-center justify-center bg-gray-400">
+    <div className="flex flex-row w-full p-4 items-center justify-center bg-gray-400 absolute">
       <RiMovie2Line size={24} />
       <p className="mx-3">Movie library</p>
-      <CustomButton title={"Sign In"} customClassName="ml-auto" />
+      <Link href={"/"} className="ml-auto">
+        <CustomButton title={"Home"} />
+      </Link>
+      <Link href={"/signin"} className="ml-2">
+        <CustomButton title={"Sign In"} customClassName="ml-auto" />
+      </Link>
     </div>
   );
 };
