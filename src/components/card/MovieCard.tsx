a1f446b9bbe7c2a32491d23a1bd9eb668600fc37
niketjain1 <niketj2000@gmail.com>
2024-06-02 17:52:40 +0530

    📦 NEW: Added search movie component

---
+++ src/components/card/MovieCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface MovieCardProps {
+  poster: string;
+  title: string;
+  year: string;
+  director: string;
+  plot: string;
+}
+
+const MovieCard = ({ poster, title, year, director, plot }: MovieCardProps) => {
+  return (
+    <div className="m-4 mb-8 w-full px-4">
+      <div className="rounded-lg bg-white shadow-lg">
+        <img src={poster} alt={title} className="w-full rounded-t-lg" />
+        <div className="p-4">
+          <h2 className="mb-2 text-lg font-semibold">{title}</h2>
+          <p className="mb-2 text-sm text-gray-700">Release year: {year}</p>
+          <p className="mb-4 text-sm text-gray-700">Director: {director}</p>
+          <a
+            href="#"
+            className="block rounded-lg bg-blue-500 px-4 py-2 text-center font-semibold text-white hover:bg-blue-600"
+          >
+            Add Movie
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
---
+++ src/components/movieComponents/SearchMovie.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { searchMovies } from "@/lib/api";
+import React, { useState } from "react";
+import MovieCard from "../card/MovieCard";
+
+type MovieResult = {
+  director: string;
+  genre: string[];
+  id: number;
+  imdbID: string;
+  imdbRating: string;
+  plot: string;
+  poster: string;
+  title: string;
+  year: string;
+};
+
+const SearchMovies: React.FC = () => {
+  const [query, setQuery] = useState("");
+  const [movie, setMovie] = useState<MovieResult>();
+
+  const handleSearch = async () => {
+    try {
+      const data = await searchMovies(query);
+      console.log(data);
+      setMovie(data);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
+
+  console.log({ movie });
+
+  return (
+    <div className="p-4">
+      <div className="flex space-x-2">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search for a movie..."
+          className="border rounded p-2 w-full"
+        />
+        <button
+          onClick={handleSearch}
+          className="bg-blue-500 text-white p-2 rounded"
+        >
+          Search
+        </button>
+      </div>
+      <div className="mt-4">
+        {movie && (
+          <MovieCard
+            title={movie.title}
+            director={movie.director}
+            plot={movie.plot}
+            poster={movie.poster}
+            year={movie.year}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SearchMovies;
