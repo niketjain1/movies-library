b867feb34654eefe8e3737490087f2b47bf87766
niketjain1 <niketj2000@gmail.com>
2024-06-03 22:42:43 +0530

    👌 IMPROVE: added log to debug

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+console.log("API URL:", API_URL);
+
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
