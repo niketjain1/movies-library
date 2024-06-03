1c870109366c0edfb1df136d1faa6b93b4a8b0b6
niketjain1 <niketj2000@gmail.com>
2024-06-03 18:14:05 +0530

    👌 IMPROVE: showing the userName if the user is logged in

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { RiMovie2Line } from "react-icons/ri";
 import CustomButton from "../button/CustomButton";
 import Link from "next/link";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const NavBar = () => {
+  const [userName, setUserName] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedUserName = localStorage.getItem("userName") as string;
+    if (storedUserName) {
+      setUserName(storedUserName);
+    }
+  }, []);
   return (
     <div className="flex flex-row w-full p-4 items-center justify-center bg-gray-400 absolute">
       <RiMovie2Line size={24} />
@@ -11,9 +22,16 @@ const NavBar = () => {
       <Link href={"/"} className="ml-auto">
         <CustomButton title={"Home"} />
       </Link>
-      <Link href={"/signin"} className="ml-2">
-        <CustomButton title={"Sign In"} customClassName="ml-auto" />
-      </Link>
+      {userName ? (
+        <>
+          <FaRegUserCircle className="ml-4 text-white" size={20} />
+          <p className="ml-2 text-white">{userName.split('"')}</p>
+        </>
+      ) : (
+        <Link href={"/signin"} className="ml-2">
+          <CustomButton title={"Sign In"} customClassName="ml-auto" />
+        </Link>
+      )}
     </div>
   );
 };
