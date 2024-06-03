1c2d1c84c301328fa0458f9fe73f52a09464ef59
niketjain1 <niketj2000@gmail.com>
2024-06-03 15:39:54 +0530

    👌 IMPROVE: fixed the moviecard

--- src/components/card/MovieCard.tsx
+++ src/components/card/MovieCard.tsx
@@ -23,14 +23,15 @@ const MovieCard = ({
   rating,
 }: MovieCardProps) => {
   return (
-    <div className="m-4 mb-8 w-64 px-4">
+    <div className="m-4 mb-8 w-80 px-4">
       <div className="rounded-lg bg-white shadow-lg">
         <img src={poster} alt={title} className="w-full rounded-t-lg" />
         <div className="p-4">
-          <h2 className="mb-2 text-lg font-semibold">{title}</h2>
-          <p className="mb-2 text-sm text-gray-700">Release year: {year}</p>
-          <p className="mb-4 text-sm text-gray-700">Director: {director}</p>
-          <p className="mb-4 text-sm text-gray-700">Rating: {rating}</p>
+          <h2 className="mb-1 text-lg font-semibold">{title}</h2>
+          <p className="mb-1 text-sm text-gray-700">Release year: {year}</p>
+          <p className="mb-1 text-sm text-gray-700">Director: {director}</p>
+          <p className="mb-1 text-sm text-gray-700">Rating: {rating}</p>
+          {/* <p className="mb-1 text-sm text-gray-700">Plot: {plot}</p> */}
           <AddMovieToList imdbID={imdbId ?? ""} onSuccess={onSucces as any} />
         </div>
       </div>
