c679765432dae2737cbd088b262f2ee923e60bae
niketjain1 <niketj2000@gmail.com>
2024-06-03 21:38:49 +0530

    👌 IMPROVE: fixed the env variable

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.PUBLIC_BACKEND_URL || "http://localhost:3000";
+const API_URL = process.env.PUBLIC_BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_URL,
