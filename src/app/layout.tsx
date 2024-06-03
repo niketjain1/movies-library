353a01bfb56ff45fbd98973dedd0b7fa04e43030
niketjain1 <niketj2000@gmail.com>
2024-06-03 14:11:23 +0530

    📦 NEW: Added react-toastify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import NavBar from "@/components/NavBar/NavBar";
+import { ToastContainer } from "react-toastify";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
+        <ToastContainer />
         <main className="flex min-h-screen min-w-screen bg-slate-500">
           {children}
         </main>
